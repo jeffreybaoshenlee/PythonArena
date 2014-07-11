7ab32e9ec7cfa16079bbb78cf13a4cc7bc71c350
ddddwww <411544043@QQ.COM>
2014-07-11 22:31:37 +0800

    尼玛的

--- src/swampy/DrawPentagon.py
+++ src/swampy/DrawPentagon.py
@@ -1,7 +1,7 @@
-from jeffrey import *
+#from jeffrey import *
 
-polygon(4,100,turtle)
-polygonWithLine(5,120,turtle)
-polygonWithLine(6,120,turtle)
-polygonWithLine(7,120,turtle)
-polygon(8,150,turtle)
+#polygon(4,100,turtle)
+#polygonWithLine(5,120,turtle)
+#polygonWithLine(6,120,turtle)
+#polygonWithLine(7,120,turtle)
+#polygon(8,150,turtle)
