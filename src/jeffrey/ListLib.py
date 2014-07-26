311683c4f18b9c548ab1301e8d2299bead8b13e1
Jeffrey Bao-Shen LEE <eastarstormlee@gmail.com>
2014-07-26 17:10:00 +0800

    簡化列表操作庫

--- src/jeffrey/ListLib.py
+++ src/jeffrey/ListLib.py
@@ -1,9 +1,6 @@
 def middle(source_list):
     result = []
     length=len(source_list)
-    if (length < 2):
-        print("The list must have at least two elements.")
-        return result
     
     for index in range(1,length-1):
         element = source_list[index]
