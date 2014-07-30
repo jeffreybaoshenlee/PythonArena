e81b6854af857554fc8aeeccf112fb0529abc4a6
huiqiangfov <huiqiangfov@huiqiangfov-Dell-System-Inspiron-M4110>
2014-07-31 01:32:40 +0800

    Modified bisect

--- src/exercises/10-11.py
+++ src/exercises/10-11.py
@@ -4,19 +4,17 @@ def bisect(word,t):
     print(len(word))
     i=int(len(word)/2)
     print(i)
-    round=0
-    round=round+1
+
     if i>0:
-        if word[i]==t[0]:
+        if word[i]==t:
             print('True')
             print(word[i])
             return word[i]
-        elif word[i]>t[0]:
-            round=round+1
+        elif word[i]>t:
             return bisect(word[:i],t)
         else:
-            rount=round+1
             return bisect(word[i:],t)        
     print('suck')    
     return False
-bisect([1,3,4,6,78,4,5,8,9,12,14,15,17,99,123,56,34,34,67,78,89,90,4],[79])        
+
+bisect([1,3,4,6,78,4,5,8,9,12,14,15,17,99,123,56,34,34,67,78,89,90,4],99)        
