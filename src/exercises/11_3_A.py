67fb6d868efb661fae3093918765204f5f9b9977
Jeffrey Bao-Shen LEE <eastarstormlee@gmail.com>
2014-08-14 00:57:31 +0800

    編寫字符頻率統計函數

--- src/exercises/11_3_A.py
+++ src/exercises/11_3_A.py
@@ -3,10 +3,10 @@ from util.WordCountLib import *
 def print_hist(histogram):
     key_list = histogram.keys()
     
-    key_list=list(key_list)
-    key_list.sort()
+    new_key_list=list(key_list)
+    new_key_list.sort()
 
-    for character in key_list:
+    for character in new_key_list:
         print(character, histogram[character])
         
 my_histogram=histogram('brontosaurus')
