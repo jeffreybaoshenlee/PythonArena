85e3d09f9643b34e479a9127d64a29f9538a50e7
Jeffrey Bao-Shen LEE <eastarstormlee@gmail.com>
2014-08-30 02:04:38 +0800

    改進函數代碼

--- src/exercises/11-9(140829).py
+++ src/exercises/11-9(140829).py
@@ -1,15 +1,15 @@
 def has_duplicates(t):
-    inverse=dict()
-    for key in t:
-        if key not in t:
-            inverse[key]=1
-        else:
+    found=dict()
+    for element in t:
+        if element in found:
             return True
-h=has_duplicates('parrot')
-print(h)
-def has_duplicates2(t):
-    a=set(t)
-    print(a)
-    return len(set(t))<len(t)
-a=has_duplicates([1,2,3])
-print(a)
+        else:
+            found[element]=1
+    
+    return False
+        
+result=has_duplicates('parrot')
+print(result)
+
+result=has_duplicates([1,2,3])
+print(result)
