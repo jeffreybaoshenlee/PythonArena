e9c22e611d6f915d0ad91dae6d103636191944fa
Jeffrey Bao-Shen LEE <eastarstormlee@gmail.com>
2014-08-30 02:42:42 +0800

    編寫代碼曝露bug，並修復bug

--- src/util/WordLib.py
+++ src/util/WordLib.py
@@ -1,7 +1,29 @@
-def rotate_word(word,number):
+def rotate_word_bug(word,number):
     result=[]
     for char in word:
         encrypted_char=ord(char)+number
         result.append(chr(encrypted_char))
         
     return ''.join(result)
+
+def rotate_word(word,number):
+    if number<0 or number>=26:
+        return word
+    
+    result=[]
+    for char in word:
+        encrypted_char=ord(char)+number
+        
+        ord_z=ord('z')        
+        if (encrypted_char>ord_z):
+            encrypted_char=encrypted_char-26
+        
+        result.append(chr(encrypted_char))
+        
+    return ''.join(result)
+
+if __name__ == '__main__':
+    print(rotate_word_bug('zigzag',12))
+    print(rotate_word('zigzag',12))
+    print(rotate_word_bug('a',26))
+    print(rotate_word('a',26))
