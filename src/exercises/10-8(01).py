9b0a7583c74bced30fe80d9ee193d89b5a523eab
Jeffrey Bao-Shen LEE <eastarstormlee@gmail.com>
2014-08-30 01:41:15 +0800

    添加又一個精簡版的判定函數

--- src/exercises/10-8(01).py
+++ src/exercises/10-8(01).py
@@ -18,9 +18,19 @@ def has_duplicates_lite(t):
             return True
 
     return False
+
+def has_duplicates_lite2(t):
+    tempSet = set(t)
+    print(tempSet)
+    
+    elementCount=len(tempSet)
+    print("The length of the Set: "+str(elementCount))
+    return elementCount<len(t)
         
 print(has_duplicates([1,2,3]))
 print(has_duplicates_lite([1,2,3]))
+print(has_duplicates_lite2([1,2,3]))
 
-print(has_duplicates([3,1,2,3]))
-print(has_duplicates_lite([3,1,2,3]))
+print(has_duplicates([3,1,2,1,3]))
+print(has_duplicates_lite([3,1,2,1,3]))
+print(has_duplicates_lite2([3,1,2,1,3]))
