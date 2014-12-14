744a5337b344ad04a517b978fe4b26e20a4b9e87
Jeffrey Bao-shen LEE <eastarstormlee@gmail.com>
2014-12-14 20:51:52 +0800

    修改代碼。

---
+++ src/swampy/趣学py编程练习.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="fov"
+__date__ ="$2014-12-7 17:46:01$"
+
+if __name__ == "__main__":
+    print "Hello World"
--- src/趣学PY编程/趣学第8章.py
+++
@@ -1,6 +0,0 @@
-import turtle
-t=turtle.Turtle()
-t.forward(90)
-
-import time
-time.sleep(5)
