f9bfd6decce182bdfe0fbfbe8a6e8fbaf45efdd0
ddddwww <411544043@QQ.COM>
2014-12-16 01:54:45 +0800

    suck-2014-12-16

---
+++ src/tkforfun/12-16.py
@@ -0,0 +1,17 @@
+import time
+from tkinter import *
+tk=Tk()
+canvas=Canvas(tk,width=500,height=500)
+canvas.pack()
+canvas.create_polygon(10,10,10,60,50,35)
+def movetriangle(event):
+    if event.keysym =='Up':
+        canvas.move(1,0,-3)
+    elif event.keysym=='Down':
+        canvas.move(1,0,3)
+    elif event.keysym=='Left':
+        canvas.move(1,-3,0)
+    else:
+        canvas.move(1,3,0)
+canvas.bind_all('<KeyPress-Return>',movetriangle)
+canvas.mainloop()
