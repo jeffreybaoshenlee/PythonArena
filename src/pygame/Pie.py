463b5287663d2740da02096456cf2f65aaed6fd1
jeffrey <jeffrey@jeffrey-Ubuntu-PC>
2015-06-06 02:11:23 +0800

    Implement a clock with color changing border.

--- src/pygame/Pie.py
+++ src/pygame/Pie.py
@@ -1,14 +1,17 @@
 # Circle Demo
 # Chapter 5
 
-import random, math, pygame,sys
+import math
+import pygame
 from pygame.locals import *
+import random
+import sys
 
 #main program begins
 pygame.init()
-screen = pygame.display.set_mode((600,500))
+screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption("Circle Demo")
-screen.fill((0,0,100))
+screen.fill((0, 0, 100))
 
 pos_x = 300
 pos_y = 250
@@ -28,18 +31,18 @@ while True:
     angle += 1
     if angle >= 360:
         angle = 0
-        r = random.randint(0,255)
-        g = random.randint(0,255)
-        b = random.randint(0,255)
-        color = r,g,b
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = r, g, b
 
     #calculate coordinates
-    x = math.cos( math.radians(angle) ) * radius
-    y = math.sin( math.radians(angle) ) * radius
+    x = math.cos(math.radians(angle)) * radius
+    y = math.sin(math.radians(angle)) * radius
 
     #draw one step around the circle
-    pos = ( int(pos_x + x), int(pos_y + y) )
-    pygame.draw.circle(screen, color, pos, 10, 6)
+    pos = (int(pos_x + x), int(pos_y + y))
+    pygame.draw.circle(screen, color, pos, 10, 5)
 
     
     pygame.display.update()
--- src/pygame/clockBug.py
+++ src/pygame/clockBug.py
@@ -1,102 +1,176 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 __author__ = "huiqiangfov"
 __date__ = "$2015-6-4 1:41:16$"
-import sys, random,pygame,time,math
+
+import datetime
+import math
+import pygame
 from pygame.locals import *
+import random
+import sys
 
-from datetime import datetime,date,time
+TITLE = "angle clock demo"
+FONT_SIZE = 36
 
-def print_text(font,x,y,text,color= (255,255,255)):
-    imgText= font.render(text,True,color)
-    screen.blit(imgText,(x,y))
+HORIZONTAL_RES = 600
+VERTICAL_RES = 500
 
-def wrap_angle(angle):
-    return angle %360
+FULL_CIRCLE_ANGLE = 360
 
-pygame.init()
-screen= pygame.display.set_mode((600,500))
-pygame.display.set_caption("angle clock demo")
-font= pygame.font.Font(None,36)
+HOUR_PER_HALF_DAY = 12
+DEGREE_PER_HOUR = FULL_CIRCLE_ANGLE / HOUR_PER_HALF_DAY
+MINUTE_PER_HOUR = 60
+DEGREE_PER_MINUTE = FULL_CIRCLE_ANGLE / MINUTE_PER_HOUR
+SECOND_PER_MINUTE = 60
+DEGREE_PER_SECOND = FULL_CIRCLE_ANGLE / SECOND_PER_MINUTE
 
-orange= 220,180,0
-white = 255,255,255
-yellow= 255,255,0
-pink= 255,100,100
+def set_display():
+    resolution = (HORIZONTAL_RES, VERTICAL_RES)
+    screen = pygame.display.set_mode(resolution)
+    pygame.display.set_caption(TITLE)
+    return screen
 
-pos_x = 300
-pos_y = 250
-radius = 250
-angle = 360
-angle1=360
-
-while True:
+def handle_exit():
     for event in pygame.event.get():
-        if event.type== QUIT:
+        if event.type == QUIT:
             sys.exit()
+                
     keys = pygame.key.get_pressed()
     if keys[K_ESCAPE]:
         sys.exit()
-        
-    screen.fill((0,0,100))
-    
 
-    
-    for n in range(1,13):
-        angle = math.radians(n*(360/12)-90)
-        x = math.cos(angle)* (radius-20)-10
-        y = math.sin(angle)* (radius-20)-10
-        print_text(font,pos_x+x,pos_y+y,str(n))
-        
-    today = datetime.today()
-    hours = today.hour%12
-   
-    minutes = today.minute
-    seconds = today.second
-    
-    hour_angle= wrap_angle(hours*(360/12)-90)
-  
-    hour_angle= math.radians(hour_angle)
-    hour_x = math.cos(hour_angle)*(radius-80)
-    hour_y = math.sin(hour_angle)*(radius-80)
-    target= (pos_x+hour_x,pos_y+hour_y)
-    pygame.draw.line(screen,pink,(pos_x,pos_y),target,25)
-    
-    
-    min_angle= wrap_angle(minutes * (360/60)-90)
-    min_angle= math.radians(min_angle)
-    min_x = math.cos(min_angle) * (radius-60)
-    min_y = math.sin(min_angle) * (radius-60)
-    target= (pos_x+min_x,pos_y+min_y)
-    pygame.draw.line(screen,orange,(pos_x,pos_y),target,12)
-    
-    sec_angle= wrap_angle(seconds * (360/60)-90)
-    sec_angle= math.radians(sec_angle)
-    sec_x= math.cos(sec_angle)* (radius-40)
-    sec_y= math.sin(sec_angle)*(radius-40)
-    target= (pos_x+sec_x,pos_y+sec_y)
-    pygame.draw.line(screen,yellow,(pos_x,pos_y),target,6)
-    
-    angle1+=1
-    if angle1 >=360:
-        angle1=0
-        r=random.randint(0,255)
-        g=random.randint(0,255)
-        b=random.randint(0,255)
-        color1=r,g,b
-    x1=math.cos(math.radians(angle1))*radius
-    y1 =math.sin(math.radians(angle1))*radius
-    
-    pos1=(int(pos_x+x1),int(pos_y+y1))
-   
-    pygame.draw.circle(screen,color1,pos1,10,0)
-    
-    pygame.draw.circle(screen,white,(pos_x,pos_y),20)
-    
-    print_text(font,0,0,str(hours)+":"+str(minutes)+":"+str(seconds))
-    
+BORDER_RADIUS = VERTICAL_RES / 2
+TEXT_RADIUS = BORDER_RADIUS - 20
+TEXT_TO_DRAW_POS_OFFSET = -10
 
+CENTER_X = HORIZONTAL_RES // 2
+CENTER_Y = VERTICAL_RES // 2
+CENTER = CENTER_X, CENTER_Y
+
+HAND_START_DEGREE = -90
+
+def hour_degree(hour):
+    return hand_degree(hour, DEGREE_PER_HOUR)
+
+def minute_degree(minute):
+    return hand_degree(minute, DEGREE_PER_MINUTE)
+
+def second_degree(second):
+    return hand_degree(second, DEGREE_PER_SECOND)
+
+def hand_degree(unit_count, degree_per_unit):
+    return unit_count * degree_per_unit + HAND_START_DEGREE
+
+def draw_clock_face():
+    for hour in range(1, HOUR_PER_HALF_DAY + 1):
+        text_angle = math.radians(hour_degree(hour))
+        
+        text_to_center_x_offset = math.cos(text_angle) * TEXT_RADIUS
+        text_x = CENTER_X + text_to_center_x_offset + TEXT_TO_DRAW_POS_OFFSET
+        
+        text_to_center_y_offset = math.sin(text_angle) * TEXT_RADIUS
+        text_y = CENTER_Y + text_to_center_y_offset + TEXT_TO_DRAW_POS_OFFSET
+        
+        print_text(text_x, text_y, str(hour))
+
+WHITE = 255, 255, 255
+
+def print_text(x, y, text, color=WHITE):
+    text_img = font.render(text, True, color)
+    screen.blit(text_img, (x, y))
+
+def draw_hand(degree, radius, color, width):
+    radians = math.radians(degree)
+    hand_to_center_x_offset = math.cos(radians) * radius
+    hand_x = hand_to_center_x_offset + CENTER_X
+    hand_to_center_y_offset = math.sin(radians) * radius
+    hand_y = hand_to_center_y_offset + CENTER_Y
+    hand_pos = (hand_x, hand_y)
+    pygame.draw.line(screen, color, CENTER, hand_pos, width)
+
+BACKGROUND_COLOR = (0, 0, 100)
+
+ORANGE = 220, 180, 0
+YELLOW = 255, 255, 0
+PINK = 255, 100, 100
+
+HOUR_RADIUS = BORDER_RADIUS - 80
+HOUR_HAND_COLOR = PINK
+HOUR_HAND_WIDTH = 25
+
+MINUTE_RADIUS = BORDER_RADIUS - 60
+MINUTE_HAND_COLOR = ORANGE
+MINUTE_HAND_WIDTH = 12
+
+SECOND_RADIUS = BORDER_RADIUS - 40
+SECOND_HAND_COLOR = YELLOW
+SECOND_HAND_WIDTH = 6
+
+BORDER_HALF_WIDTH = 10
+BORDER_SMALL_CIRCLE_WIDTH = 6
+
+current_draw_degree = FULL_CIRCLE_ANGLE
+border_color = BACKGROUND_COLOR
+last_border_color = None
+
+def draw_border(draw_degree, color):
+    draw_pos_to_center_x_offset = math.cos(math.radians(draw_degree)) * BORDER_RADIUS
+    draw_pos_x = int (CENTER_X + draw_pos_to_center_x_offset)
+    draw_pos_to_center_y_offset = math.sin(math.radians(draw_degree)) * BORDER_RADIUS
+    draw_pos_y = int (CENTER_Y + draw_pos_to_center_y_offset)
+    draw_pos = draw_pos_x, draw_pos_y
+    pygame.draw.circle(screen, color, draw_pos, BORDER_HALF_WIDTH, BORDER_SMALL_CIRCLE_WIDTH)
+
+def advance_draw_degree():
+    global current_draw_degree
+    current_draw_degree += 1
+    
+    if current_draw_degree >= FULL_CIRCLE_ANGLE:
+        current_draw_degree = 0
+
+        global last_border_color
+        global border_color
+        last_border_color = border_color
+        border_color = pick_random_color()
+
+def pick_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    
+    return (r, g, b)
+
+PIVOT_COLOR = WHITE
+PIVOT_RADIUS = 20
+
+pygame.init()
+screen = set_display()
+font = pygame.font.Font(None, FONT_SIZE)
+
+while True:
+    handle_exit()
+
+    screen.fill(BACKGROUND_COLOR)
+    draw_clock_face()
+
+    current_time = datetime.datetime.now()
+    
+    hours = current_time.hour % HOUR_PER_HALF_DAY
+    draw_hand(hour_degree(hours), HOUR_RADIUS, HOUR_HAND_COLOR, HOUR_HAND_WIDTH)
+
+    minutes = current_time.minute
+    draw_hand(minute_degree(minutes), MINUTE_RADIUS, MINUTE_HAND_COLOR, MINUTE_HAND_WIDTH)
+    
+    seconds = current_time.second
+    draw_hand(second_degree(seconds), SECOND_RADIUS, SECOND_HAND_COLOR, SECOND_HAND_WIDTH)
+    
+    advance_draw_degree()
+    for degree in range(0, current_draw_degree):
+        draw_border(degree, border_color)
+    for degree in range(current_draw_degree, FULL_CIRCLE_ANGLE):
+        draw_border(degree, last_border_color)
+
+    pygame.draw.circle(screen, PIVOT_COLOR, CENTER, PIVOT_RADIUS)
+    
+    print_text(0, 0, str(hours) + ":" + str(minutes) + ":" + str(seconds))
 
     pygame.display.update()
