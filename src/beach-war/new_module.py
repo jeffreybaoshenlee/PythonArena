c2f29b5aa9249664b6458d3615264be1960be858
huiqiangfov <huiqiangfov@huiqiangfov-Dell-System-Inspiron-M4110>
2015-06-27 12:10:14 +0800

    !

---
+++ src/beach-war/new_module.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "huiqiangfov"
+__date__ = "$2015-6-27 12:08:36$"
+
+if __name__ == "__main__":
+    print "Hello World"
