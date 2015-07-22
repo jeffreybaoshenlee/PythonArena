9b50d3b9e31c9f46ea88808157d161bfcd697428
jeffrey <jeffrey@jeffrey-Ubuntu-PC>
2015-07-22 11:37:25 +0800

    Add some examples to demonstrate the usage of the malloc module.

--- src/nerdlab/try_memory.py
+++ src/nerdlab/try_memory.py
@@ -6,7 +6,7 @@ import os
 
 class MyObject(object):
     def __init__(self):
-        self.x = os.urandom(10000)
+        self.x = os.urandom(20000)
         self.y = hashlib.sha1(self.x).hexdigest()
 
 def get_data():
@@ -33,3 +33,18 @@ if __name__ == "__main__":
     print('%d objects after' % len(found_objects))
     for obj in found_objects[:3]:
         print(repr(obj)[:100])
+    
+    import tracemalloc
+    tracemalloc.start(10) # Save up to 10 stack frames
+
+    time1 = tracemalloc.take_snapshot()
+    x = run()
+    time2 = tracemalloc.take_snapshot()
+    stats = time2.compare_to(time1, 'lineno')
+ 
+    for stat in stats:
+        print(stat)
+        
+    stats = time2.compare_to(time1, 'traceback')
+    top = stats[0]
+    print('\n'.join(top.traceback.format()))
