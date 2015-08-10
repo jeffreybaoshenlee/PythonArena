9cbbd69588d93ceb2d928fdab4d86887cfe870b7
jeffrey <jeffrey@jeffrey-Ubuntu-PC>
2015-08-10 17:06:36 +0800

    Fix the bug of drawing sprite before updating.

--- src/pygame/ZombieMob/12bug.py
+++ src/pygame/ZombieMob/12bug.py
@@ -1,38 +1,34 @@
-import random,sys,pygame,math,itertools
+from MyLibrary import *
+import pygame
 from pygame.locals import *
-from  MyLibrary import * 
+import sys
 
 pygame.init()
-
-screen= pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("12")
+timer = pygame.time.Clock()
 
-timer= pygame.time.Clock()
 player_group = pygame.sprite.Group()
-player= MySprite()
-player.load("farmer walk.png",96,96,8)
-player.position=80,80
+player = MySprite()
+player.load("farmer walk.png", 96, 96, 8)
+print('start3')
+player.position = 80, 80
 player_group.add(player)
 
-
-
 while True:
-    
     timer.tick(30)
     ticks = pygame.time.get_ticks()
     
     for event in pygame.event.get():
-      if  event.type ==QUIT :sys.exit()
-      keys = pygame.key.get_pressed()
-      if keys[K_ESCAPE]:sys.exit()
-    screen.fill((50,50,100))
-
+        if  event.type == QUIT:
+            sys.exit()
+            
+        keys = pygame.key.get_pressed()
+        if keys[K_ESCAPE]:
+            sys.exit()
+            
+    screen.fill((50, 50, 100))
+    player_group.update(ticks, 50)
     player_group.draw(screen)
     
-    pygame.display.update
-
-
-
-
-
-
+    pygame.display.update()
