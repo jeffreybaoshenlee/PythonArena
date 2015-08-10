45ec64f3f05c8e1fe7fa5ee35ce4537b74e4cd6c
huiqiangfov <huiqiangfov@huiqiangfov-Dell-System-Inspiron-M4110>
2015-08-10 11:55:54 +0800

    do not give in

---
+++ src/pygame/ZombieMob/12bug.py
@@ -0,0 +1,38 @@
+import random,sys,pygame,math,itertools
+from pygame.locals import *
+from  MyLibrary import * 
+
+pygame.init()
+
+screen= pygame.display.set_mode((800,600))
+pygame.display.set_caption("12")
+
+timer= pygame.time.Clock()
+player_group = pygame.sprite.Group()
+player= MySprite()
+player.load("farmer walk.png",96,96,8)
+player.position=80,80
+player_group.add(player)
+
+
+
+while True:
+    
+    timer.tick(30)
+    ticks = pygame.time.get_ticks()
+    
+    for event in pygame.event.get():
+      if  event.type ==QUIT :sys.exit()
+      keys = pygame.key.get_pressed()
+      if keys[K_ESCAPE]:sys.exit()
+    screen.fill((50,50,100))
+
+    player_group.draw(screen)
+    
+    pygame.display.update
+
+
+
+
+
+
