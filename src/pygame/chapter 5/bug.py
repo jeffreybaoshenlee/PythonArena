d6480b53f6fb35f12223360724422228f05a7c3e
huiqiangfov <huiqiangfov@huiqiangfov-Dell-System-Inspiron-M4110>
2015-09-13 01:10:09 +0800

    do not give in-suck

--- src/pygame/chapter 5/bug.py
+++ src/pygame/chapter 5/bug.py
@@ -46,12 +46,12 @@ class Sourrd(pygame.sprite.Sprite):
     def wrap_angle(angle):
         return angle % 360
     
-    def turn(self, filename):
+    def turn(self, filename,angle):
         self.master_image = pygame.image.load(filename).convert_alpha()
-        self.another = self.angle-1
+        
         print("another="+str(self.another))
   
-        self.another = self.angle-1
+        self.another = self.angle-angle
         self.angle = Sourrd.wrap_angle(self.another)
         print("angle="+str(self.angle))
         self.pos.x = math.sin(math.radians(self.angle)) * self.radius
@@ -62,7 +62,9 @@ class Sourrd(pygame.sprite.Sprite):
         self.rangled = -self.wrap_angle(math.degrees(self.rangle))
         print(self.rangled)
         self.turn_image = pygame.transform.rotate(self.master_image, self.rangled)
-
+        
+        self.old_pos.x = self.pos.x
+        self.old_pos.y = self.pos.y
 a = Sourrd()
 
 while True:
@@ -71,12 +73,10 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[K_ESCAPE]:sys.exit()
     screen.fill((100, 100, 100))
-    a.turn("military.png")
+    a.turn("military.png",3)
     width, height = a.turn_image.get_size()
     x = 400 + a.pos.x-width // 2
     y = 300 + a.pos.y -height // 2
     print("x=" + str(x) + ";y=" + str(y))
     screen.blit(a.turn_image, (x, y))
     pygame.display.update()
-    a.old_pos.x = a.pos.x
-    a.old_pos.y = a.pos.y
